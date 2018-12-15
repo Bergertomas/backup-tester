305aa14cdaba6f8d4712cb6990cd2c57072b0db3
Tomas Berger <bergertomasa@gmail.com>
2018-12-15 12:10:17 +0200

    Update writer.py

--- writer.py
+++ writer.py
@@ -8,6 +8,8 @@ class Writer:
     def start(self):
         self.filename = self.name + "_test.csv"
         self.full_path = os.path.join(self.path, self.filename)
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
         self.file = open(self.full_path, "w+")
         self.writer = csv.writer(self.file, dialect="excel", lineterminator='\n')
         head_row = ["filename", "comparison_status", "origin_path", "backup_path", "original_size",
@@ -15,7 +17,17 @@ class Writer:
         self.writer.writerow(head_row)
 
     def write(self, row):
-        self.writer.writerow(row)
+        try:
+            self.writer.writerow(row)
+        except UnicodeEncodeError:
+            new_row = []
+            for i in row:
+                if type(i) == str:
+                    new_i = i.encode("utf-8")
+                    new_row.append(new_i)
+                else:
+                    new_row.append(i)
+            self.writer.writerow(new_row)
 
     def end(self):
         self.file.close()
