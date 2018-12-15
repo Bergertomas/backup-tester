81c0a899b2777c06ffc18db3749d9d4b67296e4f
Tomas Berger <bergertomasa@gmail.com>
2018-12-15 12:09:47 +0200

    Update run_comparison.py

--- run_comparison.py
+++ run_comparison.py
@@ -1,22 +1,32 @@
 import comparer
+import os
 
 
 def run_comparison(range, finder, writer):
     counter = 1
+    true_count = 0
+    false_count = 0
+    already_checked = []
     while counter <= range:
         print("Round: ", counter)
         found_drive = finder.file_to_compare()
-        already_checked = []
         if found_drive:
             for found in found_drive:
-                if found in already_checked:
-                    print(found, " - Already Tested")
+                checked = (os.path.split(found)[0], os.path.split(found)[1])
+                if checked in already_checked:
+                    print(found, " - Already Tested", "\n")
                 else:
                     print(found)
                     comparer_d = comparer.Comparer(found, found_drive[found])
                     result = comparer_d.compare()
                     writer.write(result)
-                    print("{ File:", result[0], "|||", "Result:", result[1], "}")
-                    already_checked.append(found)
+                    if result[1] == True:
+                        true_count += 1
+                    else:
+                        false_count += 1
+                    print("{ File:", result[0], "|||", "Result:", result[1], "}", "\n")
+                    already_checked.append(checked)
         counter += 1
+    print("Done. ")
+    print("True count: ", true_count, "|", "False count: ", false_count)
     writer.end()
